9f1ce825d54a1f0082ce325635408d07b50e993b
Jonrie Morales Badiang <jonriemain@gmail.com>
2022-05-04 11:48:38 +0800

    something fix and add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+/* bars,close,navList */
 let bars = document.querySelector('#bars');
 let close = document.querySelector('#close');
 let navList = document.querySelector('.navList');
 
+ /* 2 Click event listener for navigation bar*/ 
 bars.addEventListener('click', ()=>{
 	navList.style.left="50%";
 	close.style.display="block"
